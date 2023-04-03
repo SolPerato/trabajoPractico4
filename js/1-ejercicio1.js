5ac3fe688f16ab01822c73050426558c324b8a03
Sol Perato <solperato27@gmail.com>
2023-04-02 22:46:54 -0300

    ejercicio 1- creacion del objeto y sus metodos

---
+++ js/1-ejercicio1.js
@@ -0,0 +1,12 @@
+let auto = {
+    modelo: 2018,
+    marca: "Fiat - Palio",
+    color: "Blanco",
+    encender: ()=>{
+        document.write(`auto encendido`);
+    },
+    apagar: ()=>{
+        document.write(`El auto se apagó`)
+    }
+}
+ console.log(auto);
