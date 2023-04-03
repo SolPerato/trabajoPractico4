1924e3b5cd118ceb4dca290c57008bef18a928c7
Sol Perato <solperato27@gmail.com>
2023-04-03 01:41:15 -0300

    ejercicio adicional 1

--- js/3-ejercicio3.js
+++ js/3-ejercicio3.js
@@ -1,46 +1,21 @@
-let cancion = {
-    //propiedades key: value
-    titulo: 'Muchachos', 
-    artista: 'La mosca',
-    duracion: '2:32',
-    anio: 2021,
-    album: 'mundial 2022',
-    genero: 'murga',
-    id: 3462,
-    //metodos
-    reproducir: function (){
-        console.log(this);
-        document.write(`<p>La cancion ${this.titulo} se esta reproduciendo...</p>`)
-    },
-    pausa: () =>{
-        console.log(this.titulo)
-        document.write(`<p>La cancion esta en pausa</p>`)
-    }
+class rectangulo{
+    constructor(ancho , alto){
+this.ancho = ancho; 
+this.alto = alto; 
 }
+calcularArea() {
+    return this.ancho * this.alto;
+};
 
-//mostrar objeto
-console.log(cancion);
+calcularPerimetro() {
+    return 2 * (this.ancho + this.alto);
+};
 
-//2 maneras de mostrar el objeto
-document.write(`<p>Cancion: ${cancion.titulo}</p>`);
-document.write(`<p>Genero: ${cancion.genero}</p>`);
-document.write(`<p>Duración: ${cancion['duracion']}</p>`);
-document.write(`<p>Artista: ${cancion.artista}</p>`);
 
-//modificar una propiedad
-cancion.artista = 'La mosca Tse - Tsee';
-document.write(`<p>Artista: ${cancion.artista}</p>`);
+};
 
-//agregar una propiedad
-cancion.premios = 'Disco de oro';
-document.write(`<p>Premios: ${cancion.premios}</p>`);
+let Rectangulo = new rectangulo(10 , 20);
+console.log(rectangulo.calcularArea());
+console.log(rectangulo.calcularPerimetro());
 
-//borrar una propiedad
-delete cancion.premios;
-console.log(cancion);
-document.write(`<p>Premios: ${cancion.premios}</p>`);
-
-cancion.reproducir();
-cancion.pausa();
-
-console.log(this);
+rectangulo.calcularArea();
---
+++ js/4-ejercicio4.js
@@ -0,0 +1,15 @@
+
+
+function precio(art ){
+    let frutas= ["naranjas" , "peras" ,  "peras" ,  " bananas" ];
+    let precio=[ 2,3,5,4];
+    let resultado=-1;
+    let int;
+    int=frutas.indexOf(art);
+    if(ind>=0){
+        resultado=precio[ind];
+        return resultado;
+    }
+
+    console.precio;
+}
